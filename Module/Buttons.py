270d1ecde89f8e992262c6a89fcb0d1b2afb5285
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-09 07:36:41 +0530

    update Buttons.py

--- Module/Buttons.py
+++ Module/Buttons.py
@@ -1,14 +1,11 @@
 from telebot.types import InlineKeyboardMarkup,InlineKeyboardButton,WebAppInfo
 
-markup = InlineKeyboardMarkup()
-    
 STARTMENU = InlineKeyboardMarkup(
-  keyboard=[
-    [
-      InlineKeyboardButton(text="y1",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
-      InlineKeyboardButton(text="y2",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
-      ],[
-        InlineKeyboardButton(text="y3",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
-        InlineKeyboardButton(text="y4",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
-        ]
-    ]
+  keyboard=[[
+    InlineKeyboardButton(text="y1",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
+    InlineKeyboardButton(text="y2",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
+    ],[
+    InlineKeyboardButton(text="y3",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
+    InlineKeyboardButton(text="y4",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
+    ]]
+  )
