354e13159656ecb7be84f0186deecd980c3f6fb2
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-09 08:02:25 +0530

    update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = Flask(__name__, static_url_path='/static')
 
 @bot.message_handler(commands=['start'])
 def ak(m):
-  bot.send_message(m.chat.id,text="d9ne")#,reply_markup=Buttons.HOME_PAGE)
+  bot.send_message(m.chat.id,text="d9ne",reply_markup=Buttons.HOME_PAGE)
 
 @app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
