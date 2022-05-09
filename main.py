229974e00b80c2130ef208cc90f05176eac149a1
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-09 08:14:03 +0530

    update main.py

--- main.py
+++ main.py
@@ -7,13 +7,30 @@ from flask import Flask, request, abort, send_file
 import config
 from Module import Buttons
 
+
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+
+BUTTON1 = InlineKeyboardButton(text="y1",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
+BUTTON2 = InlineKeyboardButton(text="y2",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
+BUTTON3 = InlineKeyboardButton(text="y3",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
+BUTTON4 = InlineKeyboardButton(text="y4",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
+
+CoolFonts = InlineKeyboardButton(text='Cool Fonts',callback_data='CoolFonts')
+DecorateText = InlineKeyboardButton(text='Decorate Text',callback_data='DecorateText')
+ 
+ 
+
+HOME_PAGE = InlineKeyboardMarkup([
+  [BUTTON1,DecorateText]
+  ])
+
 TOKEN = config.BOT_TOKEN
 bot = TeleBot(token=TOKEN, parse_mode="HTML")
 app = Flask(__name__, static_url_path='/static')
 
 @bot.message_handler(commands=['start'])
 def ak(m):
-  bot.send_message(m.chat.id,text="d9ne",reply_markup=Buttons.HOME_PAGE)
+  bot.send_message(m.chat.id,text="d9ne",reply_markup=HOME_PAGE)
 
 @app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
