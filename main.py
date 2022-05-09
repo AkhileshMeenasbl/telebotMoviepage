fef91d93aac5bbaecd259f294cbe6263466db793
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-09 07:25:34 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,75 +1,19 @@
-from flask import Flask, request, abort, send_file
-import telebot
-from telebot import TeleBot, types
 import os
-import config
+import telebot
 from utils import parse_init_data
+from telebot import TeleBot
+from flask import Flask, request, abort, send_file
+
+import config
+from Module import Buttons
 
 TOKEN = config.BOT_TOKEN
 bot = TeleBot(token=TOKEN, parse_mode="HTML")
 app = Flask(__name__, static_url_path='/static')
 
-@app.get('/')
-def index():
-    return send_file('static/index.html')
- 
- 
-@app.post('/submitOrder')
-def submit_order():
-    data = request.json
-    init_data = parse_init_data(token=config.BOT_TOKEN, raw_init_data=data['initData'])
-    if init_data is False:
-        return False
- 
-    query_id = init_data['query_id']
- 
-    result_text = "<b>Order summary:</b>\n\n"
-    for item in data['items']:
-        name, price, amount = item.values()
-        result_text += f"{name} x{amount} — <b>{price}</b>\n"
-    result_text += '\n' + data["totalPrice"]
- 
-    result = types.InlineQueryResultArticle(
-        id=query_id,
-        title='Order',
-        input_message_content=types.InputTextMessageContent(message_text=result_text, parse_mode='HTML'))
-    bot.answer_web_app_query(query_id, result)
-    return ''
- 
- 
 @bot.message_handler(commands=['start'])
 def ak(m):
-  markup = types.InlineKeyboardMarkup(
-    keyboard=[[
-        types.InlineKeyboardButton(
-          text="Order Food",
-          web_app=types.WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'),
-          )
-        ]]
-      )
   bot.send_message(m.chat.id,text="d9ne",reply_markup=markup)
-  
-@bot.message_handler(commands=['test'])
-def cmd_start(message: types.Message):
-    markup = types.InlineKeyboardMarkup(
-        keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text="Order Food",
-                    web_app=types.WebAppInfo(url='https://www.google.com'),
-                )
-            ]
-        ]
-    )
-    bot.send_message(message.chat.id, "<b>Hey!</b>\nYou can order food here!", reply_markup=markup)
- 
- 
-@bot.message_handler(func=lambda message: message.via_bot)
-def ordered(message: types.Message):
-    bot.reply_to(message, '<b>Thank you for your order!</b>\n(It will not be delivered)')
- 
- 
-
 
 @app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
