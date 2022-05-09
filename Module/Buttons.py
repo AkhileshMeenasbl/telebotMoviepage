e7e0690d62173478d5498d1113e44c576a6fb75d
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-05-09 07:49:06 +0530

    update Buttons.py

--- Module/Buttons.py
+++ Module/Buttons.py
@@ -1,11 +1,12 @@
+import telebot
 from telebot.types import InlineKeyboardMarkup,InlineKeyboardButton,WebAppInfo
 
-STARTMENU = InlineKeyboardMarkup(
-  keyboard=[[
-    InlineKeyboardButton(text="y1",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
-    InlineKeyboardButton(text="y2",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
-    ],[
-    InlineKeyboardButton(text="y3",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
-    InlineKeyboardButton(text="y4",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
-    ]]
-  )
+BUTTON1 = InlineKeyboardButton(text="y1",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
+BUTTON2 = InlineKeyboardButton(text="y2",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
+BUTTON3 = InlineKeyboardButton(text="y3",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/')),
+BUTTON4 = InlineKeyboardButton(text="y4",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/'))
+
+HOME_PAGE = InlineKeyboardMarkup([
+  [BUTTON1,BUTTON2],
+  [BUTTON3,BUTTON4],
+  ])
