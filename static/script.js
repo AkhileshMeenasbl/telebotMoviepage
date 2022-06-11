3a6ba73ef1c2506fb2e6b28fd0620aa3c53930d2
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-06-11 16:05:05 +0530

    update script.js

--- static/script.js
+++ static/script.js
@@ -64,6 +64,10 @@ function NewReleaseFunc(){
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
             callback(xobj.responseText);
+            try{
+              window.alert(xobj.responseText)}
+            catch(err){
+            }
           }
         };
         xobj.send(null);  
