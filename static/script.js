21d6c0060ecb8ef8ef742c98b6a1860f18101203
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-06-12 11:49:00 +0530

    update script.js

--- static/script.js
+++ static/script.js
@@ -30,75 +30,22 @@ function mainButtonClickListener() {
 }
 
 
-function NewReleaseFunchj(){
-  fetch('https://api.codetabs.com/v1/proxy?quest=https://hdmovie5.herokuapp.com/class')
-  .then(function(response) {
-    response.text().then(function(data) {
-      const Filename = String(data);
-      /*window.alert(Filename);*/
-      try{
-        const response = axios.get(Filename);
-        const item = response.data;
-        window.alert(item);
-        /*const fs = require('fs');
-        const dir = './' + String(Filename);
-        if (fs.existsSync(dir)) {
-          window.alert('Directory exists!');
-        } else {
-          window.alert('Directory not found.');
-        }*/
-        /*fetch(Filename)
-        .then(response => response.json())
-        .then(window.alert(response.responseText))
-        .then(data => window.alert(data))
-        .catch(error => window.alert(error));
-        */
-        /*$.ajax({
-          url: Filename, //the path of the file is replaced by File.json
-          dataType: "json",
-          success: function (response) {
-            console.log(response); //it will return the json array
-          }
-        });
-        window.alert("sucess" + response);*/
-        /*var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-        xobj.open('GET', Filename, true); // Replace 'appDataServices' with the path to your file
-        xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-            try{
-              window.alert(xobj.responseText)}
-            catch(err){
-            }
-          }
-        };
-        xobj.send(null);*/
-      }
-      catch(err){
-        window.alert(err.message);
-      }
-    });
-  });
-}
-
-
 function NewReleaseFunc() {
   fetch('https://api.codetabs.com/v1/proxy?quest=https://hdmovie5.herokuapp.com/class')
   .then(function(response) {
     response.text().then(function(data) {
       const Filename = data;
-      /*fetch("/static/foodItems.json")*/
+      window.alert(Filename);
+      /*fetch("/static/foodItems.json")
       fetch(data)
       .then((res) => {
         if (!res.ok) {
           throw new Error('Response not OK', res);
           }
-          return res;
-        }).then(/* your then handler */).catch((e) => {
+          return res;*/
+        /*}).then(/* your then handler *//*).catch((e) => {
           alert('There was an error!!');
-          });
+          });*/
       });
     });
   var x = document.getElementById("NewRelease-Items");
