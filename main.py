734e584b71376aa011b665fed33721777c8eaa33
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-06-12 12:43:22 +0530

    update main.py

--- main.py
+++ main.py
@@ -12,36 +12,29 @@ from Module import Buttons,GeneralTxt
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
+ia = imdb.IMDb()
 
 TOKEN = config.BOT_TOKEN
 bot = TeleBot(token=TOKEN, parse_mode="HTML")
 app = Flask(__name__, static_url_path='/static')
 
-ia = imdb.IMDb()
 
 @bot.message_handler(commands=['start'])
 def ak(m):
   bot.send_message(m.chat.id,text=GeneralTxt.Welcomemsg.format(m.chat.first_name),reply_markup=Buttons.HOME_PAGE)
 
-def getChatId(m):
-  print () 
-
 def UpdateData():
   Uniq_Id1 = uuid.uuid1()
   Uniq_Id = f"{Uniq_Id1}".replace("-","")
-  Directory = "/static"
   file_name = f"{Uniq_Id}.json"
   Data = GetMovies()
   with open(file_name, 'w') as fp:
     json.dump(Data, fp, indent=2)
   doc = open(file_name, "rb")
   Result_Data = doc.read()
-  #with open(file_name, 'rb') as doc:
-    #bot.send_document(chat_id="818396979",document=doc)
   return Result_Data
   
 def GetMovies():
-  #[<Movie id:4549714[http] title:_"Bahubali" (2008)_>, <Movie id:2631186[http] title:_Baahubali: The Beginning (2015)_>, <Movie id:4849438[http] title:_Baahubali 2: The Conclusion (2017)_>, <Movie id:13452374[http] title:_"Bahubali" Episode #1.70 (2009)_>, <Movie id:6940950[http] title:_"Bahubali" Episode #1.1 (2008)_>, <Movie id:5216536[http] title:_Hum Bahubali (2008)_>, <Movie id:6575210[http] title:_"Hubahu" (2002)_>, <Movie id:5523460[http] title:_"Baahubali: The Lost Legends" (2017)_>, <Movie id:7618900[http
   Result = {}
   search = ia.search_movie("Bahubali")
   srchrslt = f"{search}"
@@ -58,9 +51,8 @@ def GetMovies():
   return Result
   
 def html():  # Also allows you to set your own <head></head> etc
-  search = ia.search_movie("Bahubali")
+  search = ia.search_movie("desh")
   Text = f"{search}"
-  print(Text)
   return '<html><head>custom head stuff here</head><body>' + Text + '</body></html>'
 
 @app.route('/home',methods=['POST','GET'])
@@ -72,6 +64,10 @@ def index():
   except IndexError:
     app.logger.error('error:')
 
+@app.route("/akhil",methods=['POST','GET'])
+def akhil():
+  return html()
+  
 @app.route("/class",methods=['POST','GET'])
 def akhil():
   return UpdateData()
