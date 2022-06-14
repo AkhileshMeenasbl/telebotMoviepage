d548d214c5257ce78618f91c1704127bf812b646
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-06-14 13:26:53 +0530

    update script.js

--- static/script.js
+++ static/script.js
@@ -88,7 +88,8 @@ function TopMoviesName() {
         ImageBox.appendChild(CaptionText);
         var Newitem = document.getElementById("Slide-Top-Movie");
         Newitem.appendChild(ImageBox);
-        Newitem.setAttribute('style' , "background-color:red;");
+        Newitem.style.background-color = "red";
+        /*Newitem.setAttribute('style' , "background-color:red;");*/
         /*background-image: url('https://telegra.ph/file/4710c1f31d08bd315861c.jpg');
   background-size: cover;
   background-attachment: fixed;*/
