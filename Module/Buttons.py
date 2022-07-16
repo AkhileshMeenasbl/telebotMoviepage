3d0659d0bd514c8aa7bea4f7db48b119afebf9c6
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-07-16 22:38:26 +0530

    update Buttons.py

--- Module/Buttons.py
+++ Module/Buttons.py
@@ -2,11 +2,12 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 Movie = InlineKeyboardButton(text="🔍 Movie App",web_app=WebAppInfo(url=f'https://hdmovie5.herokuapp.com/home'))
+Add2Channel = InlineKeyboardButton(text="💡 Help",url='http://t.me/yourbot?startgroup=new')
 Help = InlineKeyboardButton(text="💡 Help",callback_data='CoolFonts')
 About = InlineKeyboardButton(text='About ⚠️',callback_data='CoolFonts')
 
 
 HOME_PAGE = InlineKeyboardMarkup([
-  [Movie],
+  [Movie,Add2Channel],
   [Help,About]
   ])
