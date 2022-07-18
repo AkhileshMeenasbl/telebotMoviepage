386b3936691a1082a65237d38b8f98166d6f8376
AkhileshMeenasbl <104320777+AkhileshMeenasbl@users.noreply.github.com>
2022-07-18 11:11:48 +0530

    update Search_Movie.py

--- IMDB/Search_Movie.py
+++ IMDB/Search_Movie.py
@@ -11,9 +11,35 @@ def SearchMovieByName(MovieName):
   srchrslt3 = str(srchrslt2)
   srchrslt4 = srchrslt3.split("_>, <")
   for i in srchrslt4:
+    intialResult = {}
     startid = i.find("Movie id:") + len("Movie id:")
     endid = i.find("[http]")
-    iid = i[startid:endid]
+    movieid = i[startid:endid]
     ttl = i.partition("title:_")[2]
-    Result[str(iid)] = str(ttl)
+    #Result[str(iid)] = str(ttl)
+    series = ia.get_movie(int(movieid))
+    intialResult["Title"] = str(ttl)
+    intialResult["Title"] = str(series)
+    year1=""
+      try:
+        year2 = series.data['year']
+        year1+= f"({year2})"
+      except:
+        year1+= "(N/A)"
+    intialResult["Year"] = str(year1)
+    cover2=""
+      try:
+        cover1 = series.data['cover url']
+        cover = f"{cover1}"
+        left = '@'
+        right = '.jpg'
+        lnkt = cover[cover.index(left)+len(left):cover.index(right)]
+        old = "@" + f"{lnkt}" + ".jpg"
+        mlink = cover.replace(f"{old}", "@.jpg")
+        cover2+=f"{mlink}"
+      except:
+        mlink="https://static10.tgstat.ru/channels/_0/2f/2f9bfb5854cd89d5644304dd58c05298.jpg"
+        cover2+=f"{mlink}"
+    intialResult["PosterUrl"] = str(cover2)
+    Result[str(movieid)] = intialResult
   return Result
